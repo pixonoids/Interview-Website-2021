eda8faf96b3456cb1a5d2f42d086c228c3e5cf12
Kavya-25 <90210074+Kavya-25@users.noreply.github.com>
2022-02-01 00:10:46 +0530

    contact us modal

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     <ParentCon backgroundURL={"./images/12.svg"}>
       <ColumnCon>
         <Heading text={"Contact Details"} />
-        <InputBox placeholder={"Phone Nunber"}></InputBox>
+        <InputBox placeholder={"Phone Number"}></InputBox>
         <RowCon>
           <Button type="solid" text={"Back"} to={"/college"} />
           <Button type="hollow" text={"Next"} to={"/dept"} />
