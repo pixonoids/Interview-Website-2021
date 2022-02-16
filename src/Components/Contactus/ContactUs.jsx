42601876099ced64cac7fd2100ffca3a5d49ffa7
Yajat Chaudhary <chaudharyyajat@gmail.com>
2022-02-16 23:05:40 +0530

    contactUs Modal responsive

--- src/Components/Contactus/ContactUs.jsx
+++ src/Components/Contactus/ContactUs.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import { GrClose } from "react-icons/gr";
-import {MdMail} from "react-icons/md"
+import { MdMail } from "react-icons/md";
+import { BsInstagram, BsFacebook, BsLinkedin, BsYoutube } from "react-icons/bs";
 import classes from "./ContactUs.module.css";
 export const ContactUs = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -17,27 +18,33 @@ export const ContactUs = () => {
           onClick={() => setModalIsOpen(false)}
           className={classes.CloseButton}
         />
-
-        <div className={classes.ContactUsContent}>
-          <div className={classes["Whatsapp-Number"]}>
-            <div className={classes["images-container"]}>
-              <div className={classes.Photo1}></div>
-              <div className={classes.Photo2}></div>
-            </div>
-            <div className={classes["Name-phone"]}>
-            <div>Vyomender Mehta <br />+91 6283011264</div>
-            <div>Aditya Devlash <br />+91 70184311004</div>
-            </div>
+        <div className={classes["name-container"]}>
+          <div>
+            <p>Harsh Srivastava</p>
+            <p>9992533244</p>
           </div>
-          <div className={classes["Contact-Email"]}><MdMail className={classes.mailimg}/> You can also email us for suggestions and queries.</div>
-          <div className={classes["Contact-social-icon"]}>
-            <ul className="social-icons-list">
-              <li className={classes["social-list"]}><a href="https://www.linkedin.com/company/pixonoids/" target="_blank">LINKEDIN</a></li>
-              <li className={classes["social-list"]}><a href="https://www.instagram.com/pixonoids/" target="_blank">INSTAGRAM</a></li>
-              <li className={classes["social-list"]}><a href="https://www.youtube.com/c/pixonoids" target="_blank">YOUTUBE</a></li>
-              <li className={classes["social-list"]}><a href="https://www.facebook.com/Pixonoids" target="_blank">FACEBOOK</a></li>
-            </ul>
+          <div>
+            <p>Smile Guleria</p>
+            <p>9992533244</p>
           </div>
+        </div>
+        <div className={classes["meeting-container"]}>
+          <MdMail className={classes.mailimg} /> You can also email us for
+          suggestions and queries.
+        </div>
+        <div className={classes["social-icon-container"]}>
+          <a href="https://www.linkedin.com/company/pixonoids/" target="_blank">
+            <BsLinkedin />
+          </a>
+          <a href="https://www.instagram.com/pixonoids/" target="_blank">
+            <BsInstagram />
+          </a>
+          <a href="https://www.youtube.com/c/pixonoids" target="_blank">
+            <BsYoutube />
+          </a>
+          <a href="https://www.facebook.com/Pixonoids" target="_blank">
+            <BsFacebook />
+          </a>
         </div>
       </Modal>
     </div>
